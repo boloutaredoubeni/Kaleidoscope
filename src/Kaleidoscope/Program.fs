170d9350d5a370a49d9abc3c23605f01d05720cd
Boloutare Doubeni (Tare) <tarecd@gmail.com>
2018-01-20 18:32:59 -0500

    Implement a buggy parser

--- src/Kaleidoscope/Program.fs
+++ src/Kaleidoscope/Program.fs
@@ -12,6 +12,9 @@ module Token =
   let [<Literal>] Comma = ","
   let [<Literal>] SpecialCharacters = "!%&*+-./<=>@^|~?"
 
+  let [<Literal>] Def = "def"
+  let [<Literal>] Extern = "extern"
+
 module rec AST = 
 
   (* expr - Base type for all expression nodes. *)
@@ -19,23 +22,23 @@ module rec AST =
     (* variant for numeric literals like "1.0". *)
     | Number of double
     (* variant for a binary operator. *)
-    | BinaryOperation of Op: Operator * Lhs: Expr * Rhs: Expr
+    | BinaryOperation of (Operator * Expr * Expr)
     (* variant for referencing a variable, like "a". *)
     | Variable of Name: string
     (* variant for function calls. *)
-    | Call of FunctionName: string * Arguments: seq<Expr>
+    | Call of (string * seq<Expr>)
 
     override expr.ToString() =
       match expr with
-      | BinaryOperation (Op=op; Lhs=lhs; Rhs=rhs) -> sprintf "%A %A %A" lhs op rhs
-      | Number n -> sprintf "%A : Number" n
-      | Variable identifier -> sprintf "%s : Variable" identifier 
-      | Call(FunctionName=fn;Arguments=args) ->
+      | BinaryOperation (op, lhs, rhs) -> sprintf "%s %s %s" (lhs.ToString()) (string op) (rhs.ToString())
+      | Number n -> sprintf "%f" n
+      | Variable identifier -> sprintf "%s" identifier 
+      | Call(fn, args) ->
         let argRepresentation =
           args
           |> Seq.map string
           |> String.concat ", "
-        sprintf "Call %s (%s)" fn argRepresentation
+        sprintf "%s (%s)" fn argRepresentation
   
   type Operator =
     | Plus
@@ -52,109 +55,161 @@ module rec AST =
 
     override op.ToString() = op.Code
 
+  type Prototype = string * seq<string>
 
+  type Decl =
   (* proto - This type represents the "prototype" for a function, which captures
    * its name, and its argument names (thus implicitly the number of arguments the
    * function takes). *)
-  type Prototype = Prototype of String
+    | Prototype of Prototype
 
   (* func - This type represents a function definition itself. *)
-  type Function = Function of Prototype: Prototype * Body: Expr
+    | Function of Prototype: Prototype * Body: Expr
+
+    override decl.ToString() = 
+      let prototypeToString (name, args) = 
+        let argRepresentation = String.concat " " args
+        sprintf "%s %s" name argRepresentation
+      match decl with
+      | Prototype (name, args) -> prototypeToString (name, args)
+      | Function (prototype, body) ->
+        sprintf "%s %s" (prototypeToString prototype) (body.ToString())
 
 module Lexer =
   open FParsec
   open Token
-  
-  let parseLeftParen = pstring LeftParen
-
-  let parseRightParen = pstring RightParen
-
-  let parseComma = pstring Comma
-  let isSymbolicOperatorChar = isAnyOf SpecialCharacters
-  let remainingOperatorCharsAndWhiteSpace = manySatisfy isSymbolicOperatorChar .>> spaces
 
   let parseWhiteSpace = spaces
+  
+  let parseLeftParen = pstring LeftParen .>> parseWhiteSpace
+
+  let parseRightParen = pstring RightParen .>> parseWhiteSpace
+
+  let parseComma = pstring Comma .>> parseWhiteSpace
+  let isSymbolicOperatorChar = isAnyOf SpecialCharacters
+
+
+  let remainingOperatorCharsAndWhiteSpace = manySatisfy isSymbolicOperatorChar .>> parseWhiteSpace
+
+  let parseWhiteSpace1 = spaces1
 
   let parseBetweenBrackets = between parseLeftParen parseRightParen
+
+  let parseDef = skipString Def .>> parseWhiteSpace
+
+  let parseExtern = skipString Extern .>> parseWhiteSpace
 
 module Parser =
   open FParsec
   open AST
   open Lexer
 
-  type private AST.Operator with
-  
-    member op.Precedence =
-      match op with
-      | Plus -> 20
-      | LessThan -> 10
-      | Minus -> 30
-      | Multiply -> 40
-
-    member op.Associativity =
-      match op with
-      | Plus -> Associativity.Left
-      | _ -> Associativity.Left
-
   type UserState = unit
 
-  let parseExpr, parseExprRef = createParserForwardedToRef<Expr, _>()
+  [<RequireQualifiedAccess>]
+  module private Expr =
+    type private AST.Operator with
+    
+      member op.Precedence =
+        match op with
+        | Plus -> 20
+        | LessThan -> 10
+        | Minus -> 30
+        | Multiply -> 40
 
-  let parseValue =
+      member op.Associativity =
+        match op with
+        | Plus -> Associativity.Left
+        | _ -> Associativity.Left
 
-    let parseNumber : Parser<Expr, UserState> = pfloat .>> spaces |>> Number
+    let private parseExpr, parseExprRef = createParserForwardedToRef<Expr, _>()
 
-    let parseVariable : Parser<Expr, UserState> = 
+    let parseIdentifier = 
       let options = IdentifierOptions()
       let parseIdentifierWithOptions = identifier options
-      parseIdentifierWithOptions .>> spaces |>> Variable
+      parseIdentifierWithOptions .>> parseWhiteSpace
 
-    let parseCall =
-      let parseArgs =
-        let parseExprs = sepBy parseExpr parseComma
-        between parseLeftParen parseRightParen parseExprs
-      let operator = parseVariable
-      operator .>>. parseArgs |>> (fun (fn, args) -> 
-        let (Variable name) = fn
-        Call(name, args)
+    let private parseValue =
+
+      let parseNumber : Parser<Expr, UserState> = pfloat .>> spaces |>> Number
+
+      let parseVariable = parseIdentifier |>> Variable
+
+      let parseCall =
+        let parseArgs =
+          let parseExprs = sepBy parseExpr parseComma
+          let parseArguments = between parseLeftParen parseRightParen parseExprs
+          parseArguments |>> seq
+        let operator = parseIdentifier
+        operator .>>. parseArgs |>> Call
+
+      choice [
+        parseNumber
+        attempt parseCall
+        attempt parseVariable
+      ]
+  
+    let private addSymbolicInfixOperators (op : Operator) opp =
+      let operator = 
+        InfixOperator(
+          op.Code,
+          remainingOperatorCharsAndWhiteSpace,
+          op.Precedence,
+          op.Associativity,
+          (),
+          fun _ lhs rhs -> BinaryOperation(op, lhs, rhs)
       )
+      (opp : OperatorPrecedenceParser<_, _, _>).AddOperator(operator)
+    
+    let private operatorPrecendenceParser = 
+      let parseTerm = 
+        parseValue .>> parseWhiteSpace
+        <|> parseBetweenBrackets parseExpr
+      let opp = OperatorPrecedenceParser()
+      do 
+        opp.TermParser <- parseTerm
+        addSymbolicInfixOperators LessThan opp
+        addSymbolicInfixOperators Plus opp
+        addSymbolicInfixOperators Minus opp
+        addSymbolicInfixOperators Multiply opp
+      opp
 
-    choice [
-      parseNumber
-      attempt parseCall
-      attempt parseVariable
-    ]
+    let private parseBinaryOperation = operatorPrecendenceParser.ExpressionParser
+
+    do parseExprRef := parseBinaryOperation
+
+    let parse = parseWhiteSpace >>. parseExpr
+         
+  [<RequireQualifiedAccess>]
+  module private Decl =
+
+    type private Decl with
+      static member EmptyPrototype = ("", Seq.empty)
+
+    let private parseTopLevel =
+      let parsePrototype = 
+        let parseArgument = sepBy Expr.parseIdentifier parseWhiteSpace
+        let parseArguments = between parseLeftParen parseRightParen parseArgument
+        Expr.parseIdentifier .>> parseWhiteSpace .>>. parseArguments |>> (fun (name, args) -> (name, seq args))
+      let parseDefinition = 
+        parseDef .>> parseWhiteSpace >>. parsePrototype .>>. Expr.parse |>> Function
+      let parseExtern =
+        parseExtern .>> parseWhiteSpace >>. parsePrototype |>> Prototype
+      let parseTopLevel =
+        Expr.parse |>> (fun body -> Function (Decl.EmptyPrototype, body))
+
+      choice [
+        parseDefinition
+        attempt parseExtern
+        attempt parseTopLevel
+      ]
+
+    let parse = parseTopLevel 
   
-  let addSymbolicInfixOperators (op : Operator) opp =
-    let operator = 
-      InfixOperator(
-        op.Code,
-        remainingOperatorCharsAndWhiteSpace,
-        op.Precedence,
-        op.Associativity,
-        (),
-        fun _ lhs rhs ->
-          BinaryOperation(op, lhs, rhs)
-    )
-    (opp : OperatorPrecedenceParser<_, _, _>).AddOperator(operator)
-  
-  let operatorPrecendenceParser = 
-    let parseTerm = 
-      parseValue .>> parseWhiteSpace
-      <|> parseBetweenBrackets parseExpr
-    let opp = OperatorPrecedenceParser()
-    do 
-      opp.TermParser <- parseTerm
-      addSymbolicInfixOperators LessThan opp
-      addSymbolicInfixOperators Plus opp
-      addSymbolicInfixOperators Minus opp
-      addSymbolicInfixOperators Multiply opp
-    opp
+  let parse = parseWhiteSpace >>. Decl.parse
 
-  let parseBinaryOperation = operatorPrecendenceParser.ExpressionParser
+  let run = runParserOnString parse () "" 
 
-  do parseExprRef := parseBinaryOperation
-       
   let showResult = 
     function
     | Success(result, _, _) ->
@@ -167,8 +222,6 @@ module Parser =
         printfn ""
         printfn "☠️ %s" message
         printfn ""
-
-  let run = runParserOnString parseExpr () "" 
 
 module Driver =
 
@@ -211,18 +264,26 @@ module Driver =
       runRepl ()
 
 open LLVMSharp
+open FParsec
 
 let [<Literal>] ``my-cool-jit`` = "my cool jit"
 
 let tests () = 
   [
-    "1.0"
-    "variable"
-    "45 + 3.0"
-    "s < 3"
-    "foo(y, 4.0 + z)"
-  ] 
-  |> Seq.iter (fun program -> Driver.run (new StringReader(program)))
+    // "def foo(x y) x+foo(y, 4.0);"
+    // "def foo(x y) x+y y;"
+    // "def foo(x y) x+y );"
+    "extern sin(a);"
+  ]
+  |> Seq.map (fun program ->
+    async {
+      let result = Parser.run program
+      match result with
+      | Success(tree, _,_) -> printfn "%A" tree
+      | Failure(errorMessage, _, _) -> failwith errorMessage
+    })
+  |> Async.Parallel
+  |> Async.RunSynchronously
 
 
 [<EntryPoint>]
@@ -232,7 +293,7 @@ let main _ =
     let builder = LLVM.CreateBuilder()
 
     (* Run the main "interpreter loop" now. *)
-    // tests ()
+    tests () |> ignore
     Driver.run Console.In
     LLVM.DumpModule(module')
   0
